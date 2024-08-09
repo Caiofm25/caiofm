8f1fe85ff98c4bc91969928f17252421534643c9
Caio <caiofmont1525@gmail.com>
2024-08-09 16:42:18 -0300

    ''

--- oi.ps1
+++ oi.ps1
@@ -1,6 +1,6 @@
 $wshell = New-Object -ComObject wscript.shell
 $wshell.AppActivate('C:\Windows\system32\cmd.exe') # Certifique-se de que o título da janela está correto
-Start-Sleep -Milliseconds 500
+Start-Sleep -Milliseconds 2000
 $wshell.SendKeys('% {SPACE}')
-Start-Sleep -Milliseconds 100
+Start-Sleep -Milliseconds 1500
 $wshell.SendKeys('n')
