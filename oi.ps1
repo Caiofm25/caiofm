92971381aa7d46ae2af730f940b60e50db7bf847
Caio <caiofmont1525@gmail.com>
2024-08-09 16:55:38 -0300

    ''

--- oi.ps1
+++
@@ -1,6 +0,0 @@
-$wshell = New-Object -ComObject wscript.shell
-$wshell.AppActivate('C:\Windows\system32\cmd.exe') # Certifique-se de que o título da janela está correto
-Start-Sleep -Milliseconds 2000
-$wshell.SendKeys('% {SPACE}')
-Start-Sleep -Milliseconds 1500
-$wshell.SendKeys('n')
--- oi.vbs
+++
@@ -1,2 +0,0 @@
-Set objshell = wscript.createobject("wscript.shell")
-objshell.run("D:\Users\Caio\Desktop\cursos\ADS\eu\oi.bat"),0, true
