0145f5b62616e5d89d36beb48a69bf3289a7576c
Caio <caiofmont1525@gmail.com>
2024-08-08 23:35:53 -0300

    ''

--- oi.ps1
+++ oi.ps1
@@ -1,5 +1,6 @@
 $wshell = New-Object -ComObject wscript.shell
 $wshell.AppActivate('C:\Windows\system32\cmd.exe') # Certifique-se de que o título da janela está correto
 Start-Sleep -Milliseconds 500
-$wshell.SendKeys('%{n}') # Minimize a janela
-
+$wshell.SendKeys('% {SPACE}')
+Start-Sleep -Milliseconds 100
+$wshell.SendKeys('n')
