1ec0129e0cd7b7c7bb748118a608562dc30a7e5c
Caio <caiofmont1525@gmail.com>
2024-08-09 16:49:50 -0300

    ''

---
+++ oi.bat
@@ -0,0 +1,5 @@
+@REM @echo off
+@REM powershell -ExecutionPolicy Bypass -File "D:\Users\Caio\Desktop\cursos\ADS\eu\oi.ps1"
+git add *
+git commit --allow-empty-message -m ''
+git push -f origin main
